b79020be1d9039d5eefe871534343fda45a9ea6b
saberbenali3 <64201612+saberbenali3@users.noreply.github.com>
2023-05-05 21:30:33 +0200

    FIx

--- mobile/lib/Screen/Widget/LoginPage.dart
+++ mobile/lib/Screen/Widget/LoginPage.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import '../../config.dart';
+
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -12,24 +14,55 @@ class _LoginPageState extends State<LoginPage> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
 
-  Future<void> _login() async {
+   Future<String?> _login() async {
+    final response = await http.post(Uri.parse('$BaseCnUrl/api/auth/signup'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode( {
+        "email": "sabeurbenali663@gmail.com",
+        "prenom": "sabeur",
+        "nom": "ben ali",
+        "password": "saber"
+      }),);
+
+    if (response.statusCode == 200) {
+      Map res = json.decode(response.body) as Map<String, dynamic>;
+print("ahlmaaa");
+ //     return res['token'] as String;
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      print("3assba");
+      throw Exception('Failed to register user');
+    }
+  }
+
+  Future<void> _login2() async {
     final username = _usernameController.text;
     final password = _passwordController.text;
+    print('user name log ' + username);
+    print('password ' + password);
+
     final response = await http.post(
       Uri.parse('localhost:8080/api/auth/signup'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
       body: jsonEncode(<String, String>{
-        'email': username,
-        'password': password,
+        "email": "sabeurbenali663@gmail.com",
+        "prenom": "sabeur",
+        "nom": "ben ali",
+        "password": "saber"
       }),
     );
 
     if (response.statusCode == 200) {
       // Navigate to the home screen if the authentication was successful
+      print("oui sayééé");
       Navigator.pushNamed(context, '/home');
     } else {
+      print("nonn ya sayééé");
       // Show an error message if the authentication failed
       showDialog(
         context: context,
@@ -58,20 +91,6 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                labelText: 'Username',
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-            ),
             SizedBox(height: 16.0),
             ElevatedButton(
               child: Text('Login'),
